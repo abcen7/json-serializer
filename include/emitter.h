1e4fb5d4dd13cc1b9af869c1650c12c4bd2469a0
Kirill Sermyagin <sermyagin.abcen7@gmail.com>
2025-06-12 22:48:37 +0300

    Added Emitter

--- include/emitter.h
+++ include/emitter.h
@@ -1,6 +1,34 @@
+#pragma once
+#include <stdio.h>
+#include <stdbool.h>
+#include "parser.h"
+
 typedef struct {
     char *buf;
     size_t len, cap;
-    int indent;       // уровень отступа для pretty-print
-    bool pretty;      // включить/выключить форматирование
+    short indent; // indentation-level for pretty-print
+    bool pretty; // on/off pretty-formatter
 } Emitter;
+
+// Initializes Emitter calls emit_value(v), returns null-terminated string
+char *json_serialize(JsonValue *v, bool pretty);
+
+/*
+ * Dispatcher. Depends of v->type calls:
+ * emit_null(), emit_bool(), emit_number()
+ * emit_string() (with shielding ", \, control characters)
+ * emit_array(), emit_object()
+ */
+static void emit_value(Emitter *e, JsonValue *v);
+
+// Creates open and close brackets: { }
+static void emit_object(Emitter *e, JsonValue *v);
+
+// Similarly for object, but without keys
+static void emit_array(Emitter *e, JsonValue *v);
+
+// Extends buffer if necessary and copies the string
+static void append(Emitter *e, const char *s);
+
+// Adds \n and the right amount of spaces bases on Emitter->indent
+static void indent_newline(Emitter *e);
